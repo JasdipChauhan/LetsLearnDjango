b8fc7ac41ff7d1a9bd4aa49705982c8fc7b6b23f
JasdipC <jasdip.chauhan@gmail.com>
2016-09-22 20:52:18 -0400

    practise with django’s orm

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+from .models import Course
 
 # Create your views here.
+def course_list(request):
+    courses = Course.objects.all()
+    output = ', '.join([str(courses) for course in courses])
+    return HttpResponse(output)
+
