e6ddfffc2af2ddaed7766e848be7fffb43a1bd33
JasdipC <jasdip.chauhan@gmail.com>
2016-09-25 22:33:25 -0400

    returning a list of python courses

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,6 @@ from .models import Course
 # Create your views here.
 def course_list(request):
     courses = Course.objects.all()
-    output = ', '.join([str(courses) for course in courses])
+    output = ', '.join([str(courses) for courses in courses])
     return HttpResponse(output)
 
