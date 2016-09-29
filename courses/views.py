63ac909c6206e55c4100f4f7e87c0e37f53388c8
JasdipC <jasdip.chauhan@gmail.com>
2016-09-28 23:25:18 -0400

    new templates, refactoring views

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,5 @@ from .models import Course
 # Create your views here.
 def course_list(request):
     courses = Course.objects.all()
-    output = ', '.join([str(courses) for courses in courses])
-    return HttpResponse(output)
+    return render(request, 'courses/course_list.html', {'courses_list': courses})
 
